a6be3f6572d8ed08a15e8331942a8abb3e32e932
SethMegbetor <sethmegbetor@gmail.com>
2025-09-11 22:31:11 +0000

    fix: :fire: - change navbar import in MainLayout.jsx
    change navbar import in MainLayout.jsx

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar";
 
 export default function MainLayout({ children }) {
   return (
