115e58e6878b9d9baaf0cc2f78670674757d6603
SethMegbetor <sethmegbetor@gmail.com>
2025-09-12 20:15:27 +0000

    Fix import path for Navbar component in MainLayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer";
-import Navbar from "../components/NavBar";
+import Navbar from "../components/Navbar.jsx";
 
 export default function MainLayout({ children }) {
   return (
