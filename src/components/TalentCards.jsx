f7b1ee64c72a5b7938d10f434fd232b74019ad0b
Yaw_Boateng <asantea176@gmail.com>
2025-09-15 00:44:11 +0000

    setup-fixes/ environment setup with axios and fixes on modal and header

--- src/components/TalentCards.jsx
+++ src/components/TalentCards.jsx
@@ -2,14 +2,14 @@ import TalentCard from "./TalentCard";
 
 export default function TalentCards({ talents, onViewProfile }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6  max-w-6xl mx-auto">
       {talents.map((talent) => (
         <TalentCard
-          name="Ama Osei"
-          role="Senior Frontend Developer"
-          image="path/to/image.jpg"
-          skills={["React", "TypeScript", "Next.js"]}
-          onViewProfile={() => console.log('View profile clicked for Ama Osei')}
+          key={talent.id}
+          name={talent.name}
+          role={talent.role}
+          image={talent.image}
+          onViewProfile={() => onViewProfile(talent)}
         />
       ))}
     </div>
