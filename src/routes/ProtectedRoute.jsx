0ef94d89f79ae7acd773421a63a6b46817b9e3df
34RL-01 <likee34rl@gmail.com>
2025-11-18 03:01:58 -0800

    Fix import paths for AuthContext to ensure consistent casing across components.

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Navigate, Outlet, } from "react-router-dom";
-import { AuthContext } from "../Context/AuthContext.jsx";
+import { AuthContext } from "../context/AuthContext.jsx";
 
 const ProtectedRoute = ({ role }) => {
   const { token, user } = useContext(AuthContext);
